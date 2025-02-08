673fe8f841e8303d6f67e23c47b3cfa5cc99c954
xiaojiqing <xiaojiqing@padolabs.org>
2025-02-07 23:15:54 -0800

    wasm bench

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -14,8 +14,8 @@ do
     response_size=`echo $line | awk -F: '{print $4}'`
     echo "$bandwith#$delay#$request_size#$response_size"
     ./scripts/simulate_network.sh $interface $bandwith $delay
-	./scripts/reset_network.sh $interface
 
     ./build/cpp/bench/bin/zktls_bench $1 $2 $3 $4 $request_size $response_size $bandwith $delay
+	./scripts/reset_network.sh $interface
     sleep 5
 done
